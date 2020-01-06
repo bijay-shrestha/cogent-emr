209c7b2035fc12ff63ea47fc339e272210c37b78
rupakchaulagain <rupakchaulagain@gmail.com>
2020-01-06 12:23:15 +0545

    resolve security config

--- auth-service/src/main/java/com/cogent/authservice/configuration/SecurityConfig.java
+++ auth-service/src/main/java/com/cogent/authservice/configuration/SecurityConfig.java
@@ -16,39 +16,38 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @EntityScan(basePackages = {"com.cogent.persistence.model"})
+@ComponentScan({
+        "com.cogent.genericservice.security"
+})
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @ComponentScan({
-            "com.cogent.genericservice.security"
-    })
-    public class SecurityCredentialsConfig extends WebSecurityConfigurerAdapter {
 
-        @Autowired
-        private UserDetailsService userDetailsService;
-        @Autowired
-        private JwtConfig jwtConfig;
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private JwtConfig jwtConfig;
 
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    .and()
-                    .csrf().disable()
-                    .cors()
-                    .and()
-                    .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig))
-                    .authorizeRequests()
-                    .antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()
-                    .antMatchers(HttpMethod.GET, jwtConfig.getUri()).permitAll()
-                    .anyRequest().authenticated();
-        }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf().disable()
+                .cors()
+                .and()
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig))
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()
+                .antMatchers(HttpMethod.GET, jwtConfig.getUri()).permitAll()
+                .anyRequest().authenticated();
+    }
 
-        @Override
-        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-        }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
 
-        @Bean
-        public BCryptPasswordEncoder passwordEncoder() {
-            return new BCryptPasswordEncoder();
-        }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
+
