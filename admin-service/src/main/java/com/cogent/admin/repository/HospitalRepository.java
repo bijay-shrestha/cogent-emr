1f3ed3771af67829357417eaffa78168ce8c681c
smriti.mool <smriti.mool@f1soft.com>
2020-01-16 09:43:40 +0545

    add @param in hospital repository

--- admin-service/src/main/java/com/cogent/admin/repository/HospitalRepository.java
+++ admin-service/src/main/java/com/cogent/admin/repository/HospitalRepository.java
@@ -4,6 +4,7 @@ import com.cogent.admin.repository.custom.HospitalRepositoryCustom;
 import com.cogent.persistence.model.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -12,8 +13,8 @@ import java.util.Optional;
  */
 public interface HospitalRepository extends JpaRepository<Hospital, Long>, HospitalRepositoryCustom {
     @Query("SELECT h FROM Hospital h WHERE h.status!='D' AND h.id = :id")
-    Optional<Hospital> findHospitalById(Long id);
+    Optional<Hospital> findHospitalById(@Param("id") Long id);
 
     @Query("SELECT h FROM Hospital h WHERE h.status='Y' AND h.id = :id")
-    Optional<Hospital> findActiveHospitalById(Long id);
+    Optional<Hospital> findActiveHospitalById(@Param("id") Long id);
 }
